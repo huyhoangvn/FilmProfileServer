aa9b6790a64a5d1e91ff91c748b86e1b76326452
phuc2062003 <127515689+phucnxph29170@users.noreply.github.com>
2023-09-22 19:49:30 +0700

    uploads
    7:49 22/9/2023

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,32 @@ const {} = require("express");
 var mongo = require('mongoose');
 const {log} = require("debug");
 const {route} = require("express/lib/router");
+const multer = require('multer');
+var maxSize = 100*1024*1024;
 main().catch(err => console.log(err));
+
+//Luu hinh anh
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, '../uploads');
+    },
+    filename: function(req, file, cb) {
+        //dat ten file dc uploade len de khong bi trung lap
+        cb(null, file.originalname)
+    },
+});
+const Filter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg'){ // check file type to be png, jpeg, or jpg
+        cb(null, true);
+    }else{
+        cb(new Error("Ảnh không phải đuổi png nên không upload được"), false);
+    }
+    // const fileSize = parseInt(req.headers["content-length"])
+    // if (fileSize >  maxSize ){
+    //   return cb(new Error("File qua to"), false);
+    // }
+}
+var upload = multer({ storage: storage, limits:{fileSize : maxSize}, fileFilter: Filter});
 
 /* GET home page. */
 //Ket noi mongoose
@@ -13,33 +38,24 @@ async function main(){
 }
 //Khoi tao account
 const account = new mongo.Schema({
-  ten: String,
-  gmail: String,
-  matKhau: String,
-  nhapLai:String
+    ten: String,
+    gmail: String,
+    matKhau: String,
+    nhapLai:String,
+    gioiTinh:String,
+    moTa:String,
+    hinhAnh: [{type:String}]
 });
 
 //Them account
-/*router.post('/themTaiKhoan',async function (req, res) {
-      const ten = req.body.ten;
-      const gmail = req.body.gmail;
-      const matKhau = req.body.matKhau;
-      const nhapLai = req.body.nhapLai;
-      const ACCOUNT = mongo.model('TaiKhoan', account, 'profile');
-      const data = await ACCOUNT.find({});
-        await ACCOUNT.create({
-            ten: ten,
-            gmail: gmail,
-            matKhau: matKhau,
-            nhapLai: nhapLai,
-        })
-      res.render('index', { data: data, messsage:'Them thanh cong!!!'})
-    });*/
-router.post('/themTaiKhoan', async function (req, res) {
+router.post('/themTaiKhoan', upload.array('hinhAnh',2), async function (req, res, next) {
     const ten = req.body.ten
     const gmail = req.body.gmail
     const matKhau = req.body.matKhau
     const nhapLai = req.body.nhapLai
+    const gioiTinh = req.body.gioiTinh
+    const moTa = req.body.moTa
+    const hinhAnh = req.files.map(file => file.filename);
     console.log( req)
     const ACCOUNT = mongo.model('TaiKhoan', account, 'profile')
     const data = await ACCOUNT.find();
@@ -47,10 +63,28 @@ router.post('/themTaiKhoan', async function (req, res) {
         ten:ten,
         gmail:gmail,
         matKhau:matKhau,
-        nhapLai:nhapLai
+        nhapLai:nhapLai,
+        gioiTinh:gioiTinh,
+        moTa:moTa,
+        hinhAnh: hinhAnh
     })
     res.render('index', {data: data, message:'Them thanh cong'})
-})
+},
+    async function (err,req, res,next ) {
+        console.log(err)
+        const ACCOUNT = mongo.model('TaiKhoan', account, 'profile')
+        const data = await ACCOUNT.find();
+        if (err.code === 'LIMIT_FILE_SIZE') {
+            res.render('index', { message: 'Vượt quá dung lượng file cho phép (100MB)' , data: data});
+        } else if (err.code === 'LIMIT_FILE_COUNT') {
+            res.render('index', { message: 'Vượt quá số lượng file tải lên đồng thời (2 File)', data: data});
+        }
+        else {
+            res.render('index', { message: err.message, data: data});
+        }
+        next(err);
+    }
+    );
 
 router.get('/', async function (req, res, next) {
   const ACCOUNT = mongo.model('TaiKhoan', account, 'profile');
