6fb2d320461a45a08eba8f3068bcce165038544a
phuc2062003 <127515689+phucnxph29170@users.noreply.github.com>
2023-09-27 14:28:56 +0700

    27/9/2023 change findOne

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,12 @@ const mongo = require('mongoose');
 const NguoiDung = require('../database/NguoiDung')
 const MulterConfigs = require("../config/MulterConfigs");
 
-//
+//api đăng kí
+//vd: taiKhoan: admin, matKhau: a
+//nếu chưa tồn tại tài khoản này thì hiện ra JSON data: bao gồm thông tin cá nhân và message đăng kí thành công
+//nếu đã tồn tại tài khoản thì sẽ trả về data rỗng và message đăng ký thất bại
+//link local: http://localhost:3002/api/themTaiKhoan
+//linh glitch: https://gratis-dusty-cabinet.glitch.me/api/themTaiKhoan
 router.post('/themTaiKhoan', MulterConfigs.upload.array('hinhAnh',1), async function (req, res) {
         const taiKhoan = req.body.taiKhoan
         const matKhau = req.body.matKhau
@@ -24,28 +29,37 @@ router.post('/themTaiKhoan', MulterConfigs.upload.array('hinhAnh',1), async func
                 hinhAnh: hinhAnh,
                 trangThai:1
             }).then(result => {objId = result._id})
-            const data = await NguoiDung.find({_id: objId});
+
+            const query = NguoiDung.where({_id:objId})
+            const data = await query.findOne();
+
             res.end(JSON.stringify({
-                id:data[0]._id,
-                hoTen:data[0].hoTen,
-                ngaySinh:data[0].ngaySinh,
-                gioiTinh:data[0].gioiTinh,
-                moTa:data[0].moTa,
-                hinhAnh:data[0].hinhAnh,
-                trangThai:data[0].trangThai,
+                id:data._id,
+                hoTen:data.hoTen,
+                ngaySinh:data.ngaySinh,
+                gioiTinh:data.gioiTinh,
+                moTa:data.moTa,
+                hinhAnh:data.hinhAnh,
+                trangThai:data.trangThai,
                 message:'Dang ki thanh cong'}));
         }else{
             res.end(JSON.stringify({data: {}, message: "Tai khoan da ton tai"}));
         }
     });
+
+
+//api đăng nhập
+//VD: taiKhoan: Quyet, matKhau: Quyet
+//Tìm kiếm trong database theo tài khoản mật khẩu
+//Nếu đúng tài khoản và mật khẩu thì cho phép đăng nhập và trả về thông tin cá nhân của tài khoản đăng nhập
+//Nếu sai thì sẽ hiện ra message sai thông tin đăng nhập
 router.post('/dangNhap', async function (req, res,next) {
     const username = req.body.taiKhoan;
     const password = req.body.matKhau;
-    console.log(req.body)
-    const query = NguoiDung.where({taiKhoan: username})
+    const query = NguoiDung.where({taiKhoan: username, matKhau: password})
     var item = await query.findOne();
-    console.log(item)
-    if (item == null){
+
+    if (item._id == null){
         res.end(JSON.stringify({data: {}, message: "dang nhap that bai"}));
     }else{
         res.end(JSON.stringify({
@@ -62,5 +76,28 @@ router.post('/dangNhap', async function (req, res,next) {
             message:"dang nhap thanh cong"}));
     }
 });
+
+//lay api thong tin ca nhan
+// router.get('/getPersonalInfos/:id', async function(req, res, next) {
+//     const id = req.params.id;
+//     const data = await NguoiDung.find({_id: id});
+//     console.log("day la data:" + data)
+//     // const mapping = await data.map((item) => {
+//     //     let urlImg;
+//     //     if (item.hinhAnh == null) {
+//     //         urlImg = ""
+//     //     } else {
+//     //         urlImg = item.hinhAnh;
+//     //     }
+//     //     return {
+//     //         id: item._id,
+//     //         hoTen: item.hoTen,
+//     //         gioiTinh: item.gioiTinh,
+//     //         ngaySinh: item.ngaySinh,
+//     //         hinhAnh: urlImg
+//     //     }
+//     // })
+//     res.end(JSON.stringify(data));
+// });
 
 module.exports = router;
